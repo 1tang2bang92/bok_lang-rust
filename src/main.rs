bd40c7111f474f99ae61ae83f0c663ce96baaa36
1tang2bang92 <shpark0906@naver.com>
2020-08-30 23:24:56 +0900

    complie option attach

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-use inkwell::*;
 use inkwell::builder::*;
 use inkwell::context::*;
 use inkwell::targets::*;
+use inkwell::*;
 
 pub mod generator;
 pub mod parser;
@@ -20,37 +20,67 @@ pub use tokenizer::*;
 pub use utils::*;
 
 fn main() {
+    //complie option
+    let mut display_lexer_output = false;
+    let mut display_parser_output = false;
+    let mut display_compiler_output = false;
+
+    //match complite option on terminal
+    for arg in std::env::args() {
+        match arg.as_str() {
+            "--dl" => display_lexer_output = true,
+            "--dp" => display_parser_output = true,
+            "--dc" => display_compiler_output = true,
+            _ => (),
+        }
+    }
+
     let context = Context::create();
 
-    let mut generator = Generator::new(&context, context.create_builder(), context.create_module("Entry"));
+    let mut generator = Generator::new(
+        &context,
+        context.create_builder(),
+        context.create_module("Entry"),
+    );
 
     let mut f = File::open("test.bs").expect("File Open Error");
 
     let mut fs = String::new();
     f.read_to_string(&mut fs).expect("File Read Error");
 
+    //lex
     let mut tokenizer = Tokenizer::new();
-    //let t = tokenizer.tokenize("if a == 10 { let b = 20 } else if a == 20 { let b = 30 } else { let b = 40 }");
     let t = tokenizer.tokenize(fs.as_ref());
-    println!("{:?}", t.clone());
+    if display_lexer_output {
+        println!("{:?}", t.clone());
+    }
+    //let t = tokenizer.tokenize("if a == 10 { let b = 20 } else if a == 20 { let b = 30 } else { let b = 40 }");
+
+    //parsing
     let mut parser = Parser::new();
     let p = parser.parse(t);
-    println!("{:?}", p.clone());
+
+    if display_parser_output {
+        println!("{:?}", p.clone());
+    }
+
+    //generate ir code
     generator.gen_code(p);
     //println!("{:?}",generator.gen_code(p).as_any_value_enum().into_pointer_value().print_to_string());
 
-    let mut module = generator.get_module();
+    if display_compiler_output {
+        let mut module = generator.get_module();
+        println!("{:?}", module.print_to_string());
+    }
     /*let triple = TargetMachine::get_default_triple();
     let triple = TargetTriple::create("x86_64-pc-win32");
     module.set_triple(&triple);
 
     let target = Target::from_triple(&triple).unwrap();
-    
     let cpu = "generic";
     let features = "";
     let level = OptimizationLevel::Default;
     let reloc_mode = RelocMode::Default;
     let code_model = CodeModel::Default;
     let targetmachine = target.create_target_machine(&triple, cpu, features, level, reloc_mode, code_model).unwrap();*/
-    println!("{:?}", module.print_to_string());
 }
